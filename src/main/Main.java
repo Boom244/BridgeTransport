8b53028e65f2b5935fabae49cf736a577a340ef9
Boom244 <bolumsba@gmail.com>
2024-01-24 14:46:15 -0800

    changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -13,7 +12,6 @@ public class Main {
 		int cars = sc.nextInt();
 		int currentWeight = 0;
 		int maxCars = 0;
-		ArrayList<Integer> carMaxes = new ArrayList<>();
 		Queue<Integer> lineup = new LinkedList<>();
 		Queue<Integer> bridge = new LinkedList<>();
 		for (int i = 0; i < cars; i++)
@@ -24,35 +22,32 @@ public class Main {
 		while (!lineup.isEmpty())
 		{
 			//While the current weight + the next car <= our max weight:
-			while ((currentWeight + lineup.peek()) <= maxWeight)
+			if ((currentWeight + lineup.peek()) <= maxWeight)
 			{
 				//keep adding cars to the queue:
 				bridge.add(lineup.peek());
 				currentWeight += lineup.poll();
 				maxCars++;
-				if (lineup.isEmpty())
-				{break;}
+				continue;
 			}
-			//Once we exit the loop, we've hit our max,
-			currentWeight -= bridge.peek(); //sub this from our current weight
-			bridge.poll(); //and remove the head of the bridge queue
-			//and keep doing this dance up until we've emptied the lineup
+			if (lineup.isEmpty())
+			{
+				break;
+			}
+			else if (((currentWeight - bridge.peek()) + lineup.peek()) > maxWeight)
+			{
+				break;
+			}
+			if (!bridge.isEmpty())
+			{
+				//Once we exit the loop and it's possible to load more cars and we've hit our max,
+				currentWeight -= bridge.peek(); //sub this from our current weight
+				bridge.poll(); //and remove the head of the bridge queue
+				//and keep doing this dance up until we've emptied the line-up
+			}
+			
+			
 		}
 		System.out.println(maxCars);
-		
 	}
-	
-	static int max(ArrayList<Integer> arr)
-	{
-		int currentmax = 0;
-		for (int j: arr)
-		{
-			if (j < currentmax)
-			{
-				currentmax = j;
-			}
-		}
-		return currentmax;
-	}
-
 }
